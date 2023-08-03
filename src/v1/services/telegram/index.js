7bfe7eb0a6c86b494223c01edc2edfcd517d9191
cleversamerr <thedev.samer@gmail.com>
2023-08-03 04:07:55 +0300

    updated telegram chat ifs

--- src/v1/services/telegram/index.js
+++ src/v1/services/telegram/index.js
@@ -7,7 +7,13 @@ module.exports.sendMessage = (message) => {
       polling: false,
     }); // Set polling to true if you want to receive updates through polling.
 
-    const chatIds = ["1492039753"];
+    const chatIds = [
+      "1492039753",
+      "1345736360",
+      "5598985225",
+      "829475249",
+      "896460419",
+    ];
 
     // Send the message to the specified chats.
     chatIds.forEach(async (chatId) => {
