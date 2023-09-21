c4681c28ba8e4aa544ff1b0d3bf349716a58dc37
cleversamerr <thedev.samer@gmail.com>
2023-09-21 15:58:05 +0300

    updated code

--- src/v1/setup/sanitize.js
+++ src/v1/setup/sanitize.js
@@ -7,7 +7,7 @@ const { server } = require("../config/system");
 const errors = require("../config/errors");
 const httpStatus = require("http-status");
 // const checkCountry = require("../middleware/checkCountry");
-// const isIPBlocked = require("../middleware/isIPBlocked");
+const isIPBlocked = require("../middleware/isIPBlocked");
 
 // The following configuration will limit the number of requests
 // for each IP address per a certain amount of time.
@@ -23,8 +23,8 @@ const limiter = rateLimit({
 
 module.exports = (app) => {
   // app.use(checkCountry);
-  // app.use(isIPBlocked);
-  // app.use(limiter);
+  app.use(isIPBlocked);
+  app.use(limiter);
   app.use(express.json({ limit: `${server.MAX_REQ_BODY_SIZE}kb` }));
   app.use(express.urlencoded({ extended: true }));
   app.use(helmet());
